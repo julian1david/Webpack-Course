6bfb8bccbb644ebbd7cdea54f5e56ab1f0a0d464
JulianF <juliandavidbf@gmail.com>
2022-05-20 18:49:38 -0500

    @feaT: update API

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -2,27 +2,30 @@ import getData from '@utils/getData.js';
 import github from '@images/github.png'
 import twitter from '@images/twitter.png'
 import instagram from '@images/instagram.png'
+import getRandomArbitrary  from '@utils/random.js'
+const random = getRandomArbitrary(0,826)
+
 
 const Template = async () => {
-  const data = await getData();
+  const data = await getData(random);
   const view = `
     <div class="About">
       <div class="card">
         <div class="card_details">
           <div class="card_photo center circle">
-            <img src="${data.picture.large}" alt="${data.name.first}">
+            <img src="${data.image}" alt="${data.name}">
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
           <p class="card_title">Hi, My name is</p>
-          <p class="card_value">${data.name.first} ${data.name.last}</p>
+          <p class="card_value">${data.name} ${data.gender}</p>
         </div>
         <div class="card_userdata">
           <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
+            <li>${data.name}@rickandmorty.com</li>
+            <li>${data.location.name}</li>
           </ul>
         </div>
         <div class="card_social">
